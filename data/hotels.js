c4105e1f03ff029bf83a3fedc0d4d8febb551c09
Claire Felt <cfeltgungatan@gmail.com>
2024-11-07 00:03:06 -0700

    ughhh

---
+++ data/hotels.js
@@ -0,0 +1,53 @@
+export const hotels = 
+[
+    {
+    "city": "Banff",
+    "photo": "House2.jpg",
+    "address": ["101 Pine Street", "Banff, AB T1L 1J3, Canada"],
+    "phone": "403-555-1010"
+    },
+    {
+    "city": "Interlaken",
+    "photo": "HouseReplace2.jpg",
+    "address": ["404 Alpenweg", "Interlaken, BE 3800, Switzerland"],
+    "phone": "+41 33 555 4040"
+    },
+    {
+    "city": "Hakone",
+    "photo": "HouseReplace4.jpg",
+    "address": ["606 Cedar Way", "Hakone, Kanagawa 250-0522, Japan"],
+    "phone": "+81 460 555 6060"
+    },
+    {
+    "city": "Asheville",
+    "photo": "House8.jpg",
+    "address": ["123 Blue Ridge Parkway", "Asheville, NC 28801"],
+    "phone": "828-555-1234"
+    },
+    {
+    "city": "Burlington",
+    "photo": "House6.jpg",
+    "address": ["789 Maple Avenue", "Burlington, VT 05401"],
+    "phone": "802-555-7890"
+    },
+    {
+    "city": "Portland",
+    "photo": "HouseReplace3.jpg",
+    "address": ["456 Cedar Lane", "Portland, OR 97201"],
+    "phone": "503-555-4567"
+    },
+    {
+    "city": "Bend",
+    "photo": "HouseReplace1.jpg",
+    "address": ["505 Redwood Boulevard", "Bend, OR 97701"],
+    "phone": "541-555-5050"
+    },
+    {
+    "city": "Spokane",
+    "photo": "HouseReplace5.jpg",
+    "address": ["789 Pinecrest Lane", "Spokane, WA 99201"],
+    "phone": "928-555-2020"
+    }
+]
+
+  
